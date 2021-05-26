70a3f0b3c723f45ade553e3cbe83a6d4224c17ee
nafiziqbal <68105275+naffiziqbal@users.noreply.github.com>
2021-05-26 16:03:28 +0600

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var img = ["img/img (1).jpg","img/img (2).jpg", "img/img (3).jpg","img/img (4).jpg", "img/img (5).jpg", "img/img (6).jpg","img/img (7).jpg","img/img (8).jpg", "img/img (9).jpg", "img/img (10).jpg","img/img (11).jpg","img/img (12).jpg"]
+var img = ["img/img (1).jpg","img/img (2).jpg","img/img (3).jpg","img/img (4).jpg","img/img (5).jpg","img/img (6).jpg","img/img (7).jpg","img/img (8).jpg","img/img (9).jpg","img/img (10).jpg","img/img (11).jpg","img/img (12).jpg"]
 var imgTag = document.querySelector("img")
 
 var count = 0
